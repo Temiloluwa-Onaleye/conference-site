a96e0168bf07e5d48c3653efa615376dc2f446c3
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-08-11 08:38:36 +0100

    filtering the speakerlist

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -7,6 +7,8 @@ import SpeakerDetail from "@/components/SpeakerDetail";
 const speakers = ({}) => {
   const [speakerList, setSpeakerList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [speakingSaturday, setSpeakingSaturday] = useState(true);
+  const [speakingSunday, setSpeakingSunday] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,28 +38,80 @@ const speakers = ({}) => {
     );
   };
 
+  const handleChangeSaturday = () => {
+    setSpeakingSaturday(!speakingSaturday);
+  };
+
+  const handleChangeSunday = () => {
+    setSpeakingSunday(!speakingSunday);
+  };
+
   isLoading ? <div>Loading...</div> : "";
+
+  const speakerListFiltered = isLoading
+    ? []
+    : speakerList
+        .filter(
+          ({ sat, sun }) => (speakingSaturday && sat) || (speakingSunday && sun)
+        )
+        .sort(function (a, b) {
+          if (a.firstName < b.firstName) {
+            return -1;
+          }
+          if (a.firstName > b.firstName) {
+            return 1;
+          }
+          return 0;
+        });
 
   return (
     <div>
       <Header />
       <Menu />
       <div className="container">
+        <div className="btn-toolbar margintopbottom5 chekbox-bigger">
+          <div className="hide">
+            <div className="form-check-inline">
+              <label className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  onChange={handleChangeSaturday}
+                  checked={speakingSaturday}
+                />
+                Saturday Speakers
+              </label>
+            </div>
+            <div className="form-check-inline">
+              <label className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  onChange={handleChangeSunday}
+                  checked={speakingSunday}
+                />
+                Sunday Speakers
+              </label>
+            </div>
+          </div>
+        </div>
         <div className="row">
           <div className="card-deck">
-            {speakerList.map(({ id, bio, firstName, lastName, favorite }) => {
-              return (
-                <SpeakerDetail
-                  key={id}
-                  id={id}
-                  bio={bio}
-                  firstName={firstName}
-                  lastName={lastName}
-                  favorite={favorite}
-                  onHeartFavoriteHandler={onHeartFavoriteHandler}
-                />
-              );
-            })}
+            {speakerListFiltered.map(
+              ({ id, bio, firstName, lastName, favorite }) => {
+                return (
+                  <SpeakerDetail
+                    key={id}
+                    id={id}
+                    bio={bio}
+                    firstName={firstName}
+                    lastName={lastName}
+                    favorite={favorite}
+                    onHeartFavoriteHandler={onHeartFavoriteHandler}
+                  />
+                );
+              }
+            )}
           </div>
         </div>
       </div>
